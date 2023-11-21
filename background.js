ad5ca56d8b59d34c5e9e718630af1fc7b79863c3
RodrigoSKohl <rodrigokohl1@yahoo.com.br>
2023-11-21 04:31:54 +0000

    agora a extensao salva o payload em cache, nao necessitando ficar atualizando mais a pagina para recarregar questoes.

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
+//Variavel que fica salvo o Payload das questões
 let lastUrlPartWithAssertions = null;
 
+//Funçao de background que captura o ultimo request da URL que retorna o JSON com as questões
 chrome.webRequest.onBeforeSendHeaders.addListener(
   function(details) {
     const url = details.url;
@@ -8,14 +10,26 @@ chrome.webRequest.onBeforeSendHeaders.addListener(
 
     if (match && match[1]) {
       lastUrlPartWithAssertions = match[1];
+      // Armazene os dados no armazenamento local
+      chrome.storage.local.set({ lastUrlPartWithAssertions: lastUrlPartWithAssertions }, function() {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+        }
+      });
     }
   },
   { urls: ["<all_urls>"] },
   ["requestHeaders"]
 );
-
+//Funçao de background que recupera o Payload salvo localmente para ficar disponivel na chamada de popup.js
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request === "getLastRequestPart") {
-    sendResponse({ urlPart: lastUrlPartWithAssertions });
+    // Recupere os dados do armazenamento local
+    chrome.storage.local.get(['lastUrlPartWithAssertions'], function(result) {
+      sendResponse({ urlPart: result.lastUrlPartWithAssertions });
+      // Certifique-se de chamar sendResponse imediatamente dentro deste bloco de callback
+    });
+    return true; // Manter a porta de mensagem aberta até que a resposta seja enviada
   }
+  return false; // Esta função de listener não lidou com a mensagem, então mantenha a porta de mensagem fechada
 });
--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 let answersLoaded = false;  // Flag para indicar se as respostas foram carregadas
 
+//Funcao que envia o GET para a url da API com o Payload capturado pelo service worker background.js + token capturado nos cookies 
 async function getCola(token, api) {
   const url = 'https://pegasus-pepe-legal.prd.us.descomplica.io/undergrad/questions/assertions-corrections?' + api;
 
@@ -23,7 +24,9 @@ async function getCola(token, api) {
     console.log('Erro:', err);
   }
 }
-
+//**EXPLOIT
+//Funçao que monta os blocos das questoes e retorna em qual posição estava true, essa posição sera a resposta 0=A, 1=B, 2=C...
+//O código funciona para qualquer quantidade de questões e respostas que as atividades possam ter
 function getCorrectPositions(data) {
   const blocks = {};
 
@@ -40,7 +43,7 @@ function getCorrectPositions(data) {
   });
 
   return blocks;
-}
+}//*EXPLOIT
 
 function displayAnswers(correctPositions) {
   const answerDiv = document.getElementById('answers');
@@ -54,29 +57,48 @@ function displayAnswers(correctPositions) {
 }
 
 document.addEventListener('DOMContentLoaded', async function() {
-  const apiDiv = document.getElementById('apiDiv');
-  const tokenDiv = document.getElementById('tokenDiv');
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    const currentTab = tabs[0];
+    
+    // Verifica se a URL da guia ativa pertence ao domínio desejado
+    if (currentTab && currentTab.url.includes('descomplica.com.br')) {
+      const apiDiv = document.getElementById('apiDiv');
+      const tokenDiv = document.getElementById('tokenDiv');
 
-  if (answersLoaded) {
-    apiDiv.innerText = '\nAPI: Respostas já carregadas';
-  } else {
-    chrome.runtime.sendMessage('getLastRequestPart', async function(response) {
-      if (response && response.urlPart) {
-        apiDiv.innerText = '\nAPI OK ';
-
-        chrome.tabs.query({ active: true, currentWindow: true }, async function(tabs) {
-          let tab = tabs[0];
-          const cookie = await chrome.cookies.get({ url: tab.url, name: 'd' });
-          if (cookie) {
-            tokenDiv.innerText = '\nToken OK ';
-            await getCola(cookie.value, response.urlPart);
+      if (answersLoaded) {
+        apiDiv.innerText = 'API: Respostas já carregadas';
+      } else {
+        chrome.runtime.sendMessage('getLastRequestPart', async function(response) {
+          if (response && response.urlPart) {
+            apiDiv.innerText = 'API OK ';
+	//Captura token que esta salvo no cookie de nome 'd'
+            const cookie = await chrome.cookies.get({ url: currentTab.url, name: 'd' });
+            if (cookie) {
+              tokenDiv.innerText = 'Token OK ';
+              await getCola(cookie.value, response.urlPart);
+            } else {
+              tokenDiv.innerText = 'Token nao encontrado.';
+            }
           } else {
-            tokenDiv.innerText = '\nToken nao encontrado.';
+            apiDiv.innerText = 'Aguardando API';
+			setTimeout(function() {
+			window.location.reload();
+			}, 2000);
           }
         });
-      } else {
-        apiDiv.innerText = '\nAguardando API';
       }
-    });
-  }
+    } else {
+
+            apiDiv.innerText = 'Este script está sendo executado fora do domínio descomplica.com.br';
+    }
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  const githubLink = document.getElementById('githubLink');
+
+  githubLink.addEventListener('click', function() {
+    chrome.tabs.create({ url: 'https://github.com/RodrigoSKohl/Descomplica-a-Vida' });
+  });
 });
